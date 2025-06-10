0b4ca868a7f1c2ba6e08c04f6d57751a64d9490d
Gustavo Muniz <gusmunizsch@gmail.com>
2025-06-10 01:22:04 -0300

    Update desktop wallpaper and configure window movement bindings in neovim

--- nvim/.config/nvim/lua/vim-options.lua
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -11,3 +11,9 @@ vim.cmd("set relativenumber")
 
 -- Configure system clipboard
 vim.opt.clipboard = "unnamedplus"
+
+-- Configure window move keybindings
+vim.keymap.set("n", "<C-h>", "<C-w>h")
+vim.keymap.set("n", "<C-j>", "<C-w>j")
+vim.keymap.set("n", "<C-k>", "<C-w>k")
+vim.keymap.set("n", "<C-l>", "<C-w>l")
