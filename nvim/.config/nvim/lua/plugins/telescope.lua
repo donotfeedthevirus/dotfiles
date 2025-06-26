8aa5271c8d3eac8e65e25faa86d3566473ebe14a
Gustavo Muniz <gusmunizsch@gmail.com>
2025-06-26 17:52:19 -0300

    Configure telescope to exclude environment folders

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -13,11 +13,33 @@ return {
 		"nvim-telescope/telescope-ui-select.nvim",
 		config = function()
 			require("telescope").setup({
+        defaults = {
+          file_ignore_patterns = {
+            "node_modules",
+            "%.git/",
+            "venv/",
+            "__pycache__/",
+          },
+          vimgrep_arguments = {
+            'rg',
+            '--line-number',
+            '--hidden',
+            '--glob=!.git/',
+            '--glob=!.node_modules/',
+            '--glob=!.venv/',
+            '--glob=!.__pycache__/',
+          }
+        },
 				extensions = {
 					["ui-select"] = {
 						require("telescope.themes").get_dropdown({}),
 					},
 				},
+        pickers = {
+          find_files = {
+            hidden = true,
+          }
+        }
 			})
 			require("telescope").load_extension("ui-select")
 		end,
