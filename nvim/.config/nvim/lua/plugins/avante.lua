8ab03df344b8b2ee1e5189875d6a3e24e31f9f31
Gustavo Muniz <gusmunizsch@gmail.com>
2025-07-02 01:07:31 -0300

    Add plugin avante.nvim for cursor like chat and editing using OpenAi's 4o-mini model

---
+++ nvim/.config/nvim/lua/plugins/avante.lua
@@ -0,0 +1,60 @@
+return {
+  "yetone/avante.nvim",
+  build = function()
+    if vim.fn.has("win32") == 1 then
+      return "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false"
+    else
+      return "make"
+    end
+  end,
+  event = "VeryLazy",
+  version = false, 
+  ---@module 'avante'
+  ---@type avante.Config
+  opts = {
+    provider = "openai",
+    providers = {
+      openai = {
+        endpoint = "https://api.openai.com/v1",
+        model = "gpt-4o-mini",
+        timeout = 20000,
+          extra_request_body = {
+            temperature = 0.25,
+            max_tokens = 512,
+          },
+      },
+    },
+  },
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "MunifTanjim/nui.nvim",
+    "echasnovski/mini.pick",
+    "nvim-telescope/telescope.nvim",
+    "hrsh7th/nvim-cmp",
+    "ibhagwan/fzf-lua",
+    "stevearc/dressing.nvim",
+    "folke/snacks.nvim",
+    "nvim-tree/nvim-web-devicons",
+    {
+      "HakonHarnes/img-clip.nvim",
+      event = "VeryLazy",
+      opts = {
+        default = {
+          embed_image_as_base64 = false,
+          prompt_for_file_name = false,
+          drag_and_drop = {
+            insert_mode = true,
+          },
+          use_absolute_path = true,
+        },
+      },
+    },
+    {
+      'MeanderingProgrammer/render-markdown.nvim',
+      opts = {
+        file_types = { "markdown", "Avante" },
+      },
+      ft = { "markdown", "Avante" },
+    },
+  },
+}
