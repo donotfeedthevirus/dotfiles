914b6be459135c2c8c822e6cf98e468db8730833
Gustavo Muniz <gusmunizsch@gmail.com>
2025-11-28 23:38:52 -0300

    feat(omarchy): Remove unused themes

--- omarchy/.config/omarchy/themes/fireside/neovim.lua
+++
@@ -1,7 +0,0 @@
-return {
-	dir = "~/.config/omarchy/themes/fireside/fireside.nvim",
-	name = "fireside",
-	config = function()
-		vim.cmd.colorscheme("fireside")
-	end,
-}
--- omarchy/.config/omarchy/themes/gold-rush/tmux.conf
+++
@@ -1,2 +0,0 @@
-set -g @plugin 'egel/tmux-gruvbox'
-set -g @tmux-gruvbox 'dark'
